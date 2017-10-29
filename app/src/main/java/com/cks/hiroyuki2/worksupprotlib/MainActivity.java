5e596b73f9aa72f30fd6a1b99b3c15b70f345f50
hiroyuki <hiroyukitamura@gmail.com>
2017-10-30 05:22:43 +0900

    メインアクティビティを作成

--- app/src/main/java/com/cks/hiroyuki2/worksupprotlib/MainActivity.java
+++ app/src/main/java/com/cks/hiroyuki2/worksupprotlib/MainActivity.java
@@ -3,10 +3,15 @@ package com.cks.hiroyuki2.worksupprotlib;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
     }
 }
