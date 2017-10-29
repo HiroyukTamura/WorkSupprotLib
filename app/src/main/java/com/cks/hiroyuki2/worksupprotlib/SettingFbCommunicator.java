54cee2122f05f1a56132116a523cad504670511b
hiroyuki <hiroyukitamura@gmail.com>
2017-10-29 22:04:36 +0900

    リントかけてデバッグ うーん。。。

--- app/src/main/java/com/cks/hiroyuki2/worksupprotlib/SettingFbCommunicator.java
+++ app/src/main/java/com/cks/hiroyuki2/worksupprotlib/SettingFbCommunicator.java
@@ -11,6 +11,7 @@ import android.support.annotation.StringDef;
 import android.support.v4.app.Fragment;
 import android.widget.Toast;
 
+
 import com.cks.hiroyuki2.worksupportlib.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
