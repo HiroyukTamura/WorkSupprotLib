67d96f88e72c07b390d04a63736d401e58b393b4
hiroyuki <hiroyukitamura@gmail.com>
2017-10-30 13:09:15 +0900

    なにかがおかしい・・・

--- app/src/main/java/com/cks/hiroyuki2/worksupprotlib/FirebaseStorageUtil.java
+++ app/src/main/java/com/cks/hiroyuki2/worksupprotlib/FirebaseStorageUtil.java
@@ -209,63 +209,6 @@ public class FirebaseStorageUtil implements OnPausedListener, OnProgressListener
     }
     //endregion
 
-//    public static void showUploadingNtf(UploadTask.TaskSnapshot taskSnapshot, String fileName, int id){
-//        String text = getApplicationContext().getString(R.string.msg_start_upload);
-//        NotificationCompat.Builder builder = createNtfBase(fileName, text, id)
-//                .setCategory(Notification.CATEGORY_PROGRESS)
-//                .setAutoCancel(false)
-//                .setProgress((int) taskSnapshot.getTotalByteCount(), (int) taskSnapshot.getBytesTransferred(), false);
-//        Notification notification = builder.build();
-//        notification.flags = Notification.FLAG_NO_CLEAR;
-//        showNtf(id, notification);
-//    }
-//
-//    public static void showDownloadingNtf(FileDownloadTask.TaskSnapshot taskSnapshot, String fileName, int id){
-//        String text = getApplicationContext().getString(R.string.msg_succeed_download);
-//        NotificationCompat.Builder builder = createNtfBase(fileName, text, id)
-//                .setCategory(Notification.CATEGORY_PROGRESS)
-//                .setAutoCancel(false)
-//                .setProgress((int) taskSnapshot.getTotalByteCount(), (int) taskSnapshot.getBytesTransferred(), false);
-//        Notification notification = builder.build();
-//        notification.flags = Notification.FLAG_NO_CLEAR;
-//        showNtf(id, notification);
-//    }
-//
-//    public static void showCompleteNtf(String fileName, int id, @StringRes int textRes){
-//        String text = getApplicationContext().getString(textRes);
-//        NotificationCompat.Builder builder = createNtfBase(fileName, text, id)
-//                .setCategory(Notification.CATEGORY_STATUS);
-//        showNtf(id, builder.build());
-//    }
-//
-//    private static PendingIntent createPendingIntent(int id){
-//        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-//        intent.setFlags(
-//                Intent.FLAG_ACTIVITY_CLEAR_TOP  // 起動中のアプリがあってもこちらを優先する
-//                        | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED  // 起動中のアプリがあってもこちらを優先する
-//                        | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS  // 「最近利用したアプリ」に表示させない
-//        );
-//        return PendingIntent.getActivity(
-//                getApplicationContext(),
-//                id,
-//                intent,
-//                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_ONE_SHOT);
-//    }
-//
-//    private static NotificationCompat.Builder createNtfBase(String fileName, String text, int id){
-//        return new NotificationCompat.Builder(getApplicationContext(), NOTIFICATION_CHANNEL)
-//                .setSmallIcon(R.drawable.ic_cloud_upload_white_24dp)
-//                .setContentTitle(fileName)
-//                .setContentText(text)
-//                .setTicker(text)
-//                .setContentIntent(createPendingIntent(id));
-//    }
-//
-//    private static void showNtf(int id, Notification ntf){
-//        NotificationManagerCompat manager = NotificationManagerCompat.from(getApplicationContext());
-//        manager.notify(id, ntf);
-//    }
-
     @Override
     public void onPaused(Object o) {
 
